ba79e00ee54875d22cdcd35541313b40e4ff76ba
Ryan Adams <ryan.g.adams@gmail.com>
2012-09-11 00:37:39 +0100

    be brief and just output the table of committer stats

--- generate.rb
+++ generate.rb
@@ -24,6 +24,7 @@ OptionParser.new do |o|
     options[:by_author] = 1
     $author = author 
   }
+  o.on('-b', "Just the author stats (be brief)") { |brief| options[:be_brief] = 1 }
 	o.on('-j', "Group by jira ticket number (shows summary)") { |jira| options[:by_jira] = 1 }
 	o.on('-l SVN LOCATION', "get the log from this location") { |log| options[:log] = log }
 	o.on('-d date', "Get commits since this date (must be Date.parse-able, defaults to last week, use 'all' to see the whole log)") { |date| 
@@ -40,4 +41,4 @@ log_list = Svnlog.fetchlog(options[:log], options[:date], $config_data)
       
 # print some stats
 print_author_stats(log_list)
-print_pretty_history(log_list, $author, options) 
+print_pretty_history(log_list, $author, options) unless options[:be_brief] == 1
